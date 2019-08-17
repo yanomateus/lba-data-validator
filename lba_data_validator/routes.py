f88c8fe028f67bb5084357898e5114976abadba3
rodolphomacedo <rodolpho.ime@gmail.com>
2019-08-17 17:52:57 -0300

    add view home

--- lba_data_validator/routes.py
+++ lba_data_validator/routes.py
@@ -1,2 +1,6 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/home')
+    def home_view(request):
+        return {'status': 'ok'} 
+    config.add_view(home_view, name='home_view', route_name='home', renderer='json')
