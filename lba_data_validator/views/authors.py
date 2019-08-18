52915fc8dad313b3aa204fd88cd2400802558b2c
Moises9 <moises.estat@gmail.com>
2019-08-18 13:52:51 -0300

    improved indentation

--- lba_data_validator/views/authors.py
+++ lba_data_validator/views/authors.py
@@ -1,15 +1,15 @@
 
 def authors_view(request):
     contributors = [{"name": "Mateus",
-                    "last_name": "Yano",
-                    "age": 26,
-                    "occupation": "Developer"},
+                     "last_name": "Yano",
+                     "age": 26,
+                     "occupation": "Developer"},
                     {"name": "Rodolpho",
-                    "last_name": "Santos",
-                    "age": 26,
-                    "occupation": "Date Scientist"},
+                     "last_name": "Santos",
+                     "age": 26,
+                     "occupation": "Date Scientist"},
                     {"name": "Moises",
                      "last_name": "Oliveira",
                      "age": 32,
-                    "occupation": "Date Analyst"}]
+                     "occupation": "Date Analyst"}]
     return contributors
