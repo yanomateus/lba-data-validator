0ba251477b14294d9bf12308f7e495a81b2d4d76
Moises9 <moises.estat@gmail.com>
2019-08-27 23:43:09 -0300

    implement button load file

--- lba_data_validator/views/data.py
+++ lba_data_validator/views/data.py
@@ -5,4 +5,4 @@ from pyramid.view import view_config
 @view_config(route_name='data', renderer='../templates/data.jinja2')
 def data_view(request):
 
-    return {'name': 'moisés'}
+    return {'Data': 'Data'}
